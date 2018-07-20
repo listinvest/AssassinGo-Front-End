0e33d19fa99f1f4079c503bcdbb8a18596364edd
U1in <714896166@qq.com>
2018-07-20 23:48:10 +0800

    Update recon.vue

--- AssassinGo-Front-End/src/pages/recon.vue
+++ AssassinGo-Front-End/src/pages/recon.vue
@@ -14,7 +14,7 @@ import Tab from '../components/Tab'
 import ReconBase from '../components/Recon-Base'
 import ReconTraceRoute from '../components/Recon-TraceRoute'
 import ReconSiteMap from '../components/Recon-SiteMap'
-import ReconDirb from '../components/Recon-DirB'
+import ReconDirb from '../components/Recon-Dirb'
 import ReconSubdomain from '../components/Recon-Subdomain'
 export default {
     components: {
